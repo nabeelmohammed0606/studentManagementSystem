c41e7ec30739d921aa66058635bf52f3bc83e519
nabeelmohammed0606 <nabeelmohammed0606@gmail.com>
2026-04-15 15:02:00 -0400

    application works

--- Student.java
+++ Student.java
@@ -1,8 +1,35 @@
-// Student Management System
+// Exercise 1: Student Management System
+class Student {
+private String studentID;
+private String name;
+private double grade;
+public Student() {
+this.studentID = "0000";
+this.name = "Unknown";
+this.grade = 0.0;
+}
+public Student(String studentID, String name, double grade) {
+this.studentID = studentID;
+this.name = name;
+this.grade = grade;
+}
+public void updateGrade(double newGrade) {
+this.grade = newGrade;
+}
+public void displayDetails() {
+System.out.println("Student ID: " + studentID);
+System.out.println("Name: " + name);
+System.out.println("Grade: " + grade);
+}
+}
+// Test Code for Student
+class StudentTest {
+public static void main(String[] args) {
+Student student1 = new Student("275195", "Nabeel", 100.5);
+student1.displayDetails();
+student1.updateGrade(90.0);
+student1.displayDetails();
 
-public class Student {
-     public static void main(String[] args) {
-        
-        System.out.print ("Hello") ;
-    }
+// create an if statenent that if the grade is over 100.0 to display: INVALID GRADE
+}
 }
