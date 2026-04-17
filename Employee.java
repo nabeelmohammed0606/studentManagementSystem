87c79272812709ceb3ca1dd45540ab1197864db7
nabeelmohammed0606 <nabeelmohammed0606@gmail.com>
2026-04-17 08:52:47 -0400

    bug almost solved

--- Employee.java
+++ Employee.java
@@ -14,7 +14,7 @@ public class Employee {
     private double salary;
     private double percentage;
 
-    public Employee(String employeeID, String name, String designation, double salary) {
+    public Employee(String employeeID, String name, String designation, String salary) {
         this.employeeID = "0000";
         this.name = "unknownName";
         this.designation = "unknownDesignation";
@@ -31,6 +31,9 @@ public void employeeDetails(String employeeID, String Name, String degisnation, 
     System.out.println("Employee ID: " + employeeID + "Employee Name: " + name + "Employee Department: " + designation + "Salary: /$" + salary);
 }
 
+//
+public void displayEmployee(){
+
 }
 
 // test for the employee
@@ -39,8 +42,8 @@ class Employeetest {
     public static void main(String[] args) {
         Employee emp1 = new Employee("E001", "Nabee Mohammed", "devsecops engineer", "11500.00");
         emp1.displayEmployee();
-        emp1.applyIncrement();
+        emp1.applyIncrement(10);
         emp1.displayEmployee();
-
     }
+}
 }
